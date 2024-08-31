e37ce160e7e1814dcbc9454d36f738f3fe59f989
zr5tux <edd@edd.za.net>
2024-08-31 17:24:24 +0200

    DevSetup

--- components/snmp/snmp_component.cpp
+++ components/snmp/snmp_component.cpp
@@ -266,7 +266,7 @@ void SNMPComponent::setup() {
   setup_chip_mib_();
   setup_wifi_mib_();
   
-snmp_agent_.addDynamicIntegerHandler(CUSTOM_OID "5.1.0","testedd");
+snmp_agent_.addDynamicIntegerHandler(CUSTOM_OID "5.1.0",33);
 
 
   snmp_agent_.sortHandlers();  // for walk to work properly
